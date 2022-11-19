0ed6375bbf056f63628ab59c95f6b759b9f8ec3b
Shivam Vijaywargi <vjshivam5@gmail.com>
2022-11-19 18:40:25 +0530

    FE: Snapshot before any CSS library

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Nav/Navbar";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
