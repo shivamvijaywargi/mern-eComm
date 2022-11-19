cdf190a827779e8325de02d196591156e8513562
Shivam Vijaywargi <vjshivam5@gmail.com>
2022-11-19 13:32:18 +0530

    BE: Added morgan and hash pass method

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 import dotenv from "dotenv";
 import express from "express";
+import morgan from "morgan";
 
 dotenv.config();
 
 const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
 
 // Importing the routes
 import authRoutes from "./routes/auth.routes.js";
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,29 +1,52 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"],
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password must be atleast 8 characters"],
+    },
+    address: {
+      type: String,
+      trim: true,
+    },
+    role: {
+      type: Number,
+      default: 0,
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-    minlength: [8, "Password must be atleast 8 characters"],
-  },
-  address: {
-    type: String,
-    trim: true,
-  },
-  role: {
-    type: Number,
-    default: 0,
-  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
 });
+
+userSchema.methods.comparePassword = async function (plainPassword) {
+  return await bcrypt.compare(plainPassword, this.password);
+};
+
+userSchema.methods.getJWTToken = async function () {
+  return await jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRY,
+  });
+};
 
 export default mongoose.model("User", userSchema);
